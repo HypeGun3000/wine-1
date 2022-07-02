c197d532156895e89f8689cb5965e4d7c426d5a3
Daniil <81660697+HypeGun3000@users.noreply.github.com>
2022-07-03 00:24:14 +0300

    done first lesson 'verstka python'

--- jinja_push.py
+++ jinja_push.py
@@ -3,6 +3,9 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from datetime import date
+from collections import defaultdict
+
+import pandas
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -20,9 +23,24 @@ elif str(current_year)[-1] in '234':
 elif str(current_year)[-1] == '1':
     current_title = 'год'
 
+excel_file = pandas.read_excel('wine.xlsx')
+excel_file2 = pandas.read_excel('wine2.xlsx')
+excel_file3 = pandas.read_excel('wine3.xlsx')
+
+types_whole_wines = excel_file.to_dict(orient='records')
+types_whole_wines2 = excel_file2.to_dict(orient='records')
+types_whole_wines3 = excel_file3.to_dict(orient='records')
+
+new_dict = defaultdict(list)
+
+for wine in types_whole_wines3:
+    new_dict[wine['Категория']].append(wine)
+
 rendered_page = template.render(
     current_year=current_year,
-    let_god_goda=current_title
+    let_god_goda=current_title,
+    types_whole_wines=types_whole_wines,
+    new_dict=new_dict
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
